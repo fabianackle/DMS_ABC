81ad8d7b6fc4f01f15694b5328ca87923dd0c7ec
Fabian Ackle <fabian.ackle@gmail.com>
2024-06-14 13:57:48 +0200

    Added functionality for parameters

--- run_dms_abc.py
+++ run_dms_abc.py
@@ -9,6 +9,13 @@ import counter
 import create_count_file
 
 
+def get_readingframes(reference_sequence, frameshift_position, frameshift_offset):
+    """Returns the two reading frames"""
+    frame1 = reference_sequence[:frameshift_position]
+    frame2 = reference_sequence[frameshift_position - frameshift_offset:]
+    return frame1, frame2
+
+
 def DMS_processing(data_dict):
     """
     Analyses curent file i from data_dict
@@ -28,10 +35,35 @@ def DMS_processing(data_dict):
 
 
 def main():
-    with open('Json_test.json', 'r') as json_file:
-        data_dict = json.load(json_file)
+    parser = argparse.ArgumentParser(description="Settings for DMS_ABC script.")
+    parser.add_argument("--bam_file")
+    parser.add_argument("--position_list", nargs='+', type=int)  # list codons to be analyzed
+    parser.add_argument("--reference_sequence") # reference fasta file
+    parser.add_argument("--readingframes", action='store_true')  # bool multiple reading frames
+    parser.add_argument("--frameshift_position", default=0, type=int)
+    parser.add_argument("--frameshift_offset", default=0, type=int)
+    args = parser.parse_args()
 
-    DMS_processing(data_dict)
+    parameters = {
+        'bam_file': args.bam_file,
+        'position_list': args.position_list,
+        'reference_sequence': args.reference_sequence,
+        'readingframes': args.readingframes,
+        'frameshift_position': args.frameshift_position,
+        'frameshift_offset': args.frameshift_offset
+    }
+
+    if bool(args.readingframes) == True:
+        position = int(args.frameshift_position)
+        offset = int(args.frameshift_offset)
+        frame1, frame2 = get_readingframes(args.reference_sequence, position, offset)
+        parameters['frameshift_position'] = position
+        parameters['frameshift_offset'] = offset
+        parameters['frame1'] = frame1
+        parameters['frame2'] = frame2
+
+
+    DMS_processing(parameters)
 
 
 if __name__ == "__main__":
